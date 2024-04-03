44b486c949e63c1cdc2568ac49d5d7b46b5d8f8c
savorli <ols@trustpilot.com>
2024-04-03 19:44:16 +0200

    dist -> docs

--- app/copy-public.cjs
+++ app/copy-public.cjs
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const distPath = path.resolve(__dirname, '..', 'app', 'public');
-const parentDir = path.resolve(__dirname, '..', 'dist', 'public');
+const parentDir = path.resolve(__dirname, '..', 'docs', 'public');
 
 fs.copy(distPath, parentDir)
   .then(() => console.log('Files copied successfully!'))
