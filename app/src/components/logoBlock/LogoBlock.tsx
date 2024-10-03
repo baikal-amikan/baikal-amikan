39610c07d8409ff025aa20f6c18cb851bb1324cf
savorli <ols@trustpilot.com>
2024-10-03 08:49:12 +0200

    logo moved

--- app/src/components/logoBlock/LogoBlock.tsx
+++ app/src/components/logoBlock/LogoBlock.tsx
@@ -1,12 +1,17 @@
 import css from "./LogoBlock.module.scss";
 import { Link } from "react-router-dom";
 import {FILES_PATH, useSafeContext} from "../../config.ts";
-import { BaseContext } from "../../contexts/BaseContext.tsx";
+import {BaseContext} from "../../contexts/BaseContext.tsx";
 
-export default function LogoBlock() {
+interface LogoBlockProps {
+  position?: "center" | "left";
+}
+
+export default function LogoBlock({ position }: LogoBlockProps) {
   const { language } = useSafeContext(BaseContext);
+  const cssClass = position ? css[position] : css.left;
   return (
-    <Link to={`/${language}/`} className={css.logoImage}>
+    <Link to={`/${language}/`} className={`${css.logoImage} ${cssClass}`}>
       <img srcSet={`${FILES_PATH}/logo.png`} src={`${FILES_PATH}/logo.png`} alt={"Baikal-Amikan"} />
     </Link>
   );
